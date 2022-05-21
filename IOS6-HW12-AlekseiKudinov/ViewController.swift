0ae010e1e11a054656d3d8f3d5e2658259c0cfce
AlekseiKudinov <machette94@gmail.com>
2022-05-21 12:39:35 +0300

    Initial Commit

---
+++ IOS6-HW12-AlekseiKudinov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS6-HW12-AlekseiKudinov
+//
+//  Created by Алексей Кудинов on 21.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
